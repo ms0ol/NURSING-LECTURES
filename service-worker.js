c52aace0a36a6db7244ec73b91e8983dd2fb47b5
ms0ol <kkiikk817@gmail.com>
2026-02-16 22:10:44 +0300

    Create service-worker.js for PWA offline functionality and caching

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// Install Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('my-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/script.js',
+                '/images/logo.png',
+                // List other assets you want to cache
+            ]);
+        })
+    );
+});
+
+// Fetch Event
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+    const cacheWhitelist = ['my-cache'];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
